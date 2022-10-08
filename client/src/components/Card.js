bb33bf985979d802cf1c0de02af1bef065d27c48
ossarol <ossi.arola@qpr.com>
2022-10-08 12:28:46 +0300

    statsit

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,6 +2,10 @@ export default function Card({ veggie, onSelect }) {
     return (
         <div className={`vegCard`} onDoubleClick={() => onSelect(veggie.id)}>
             <h3>{veggie.name}</h3>
+            <p>Health: {veggie.energyKcal}</p>
+            <p>Attack: {veggie.carbohydrate}</p>
+            <p>Defence: {veggie.protein}</p>
+            <p>Initiative: {veggie.fat} </p>
         </div>
     )
 }
