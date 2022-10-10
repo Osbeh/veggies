c6117f4b365685e5108ef73675b07ba8aeb62ed1
ossarol <ossi.arola@qpr.com>
2022-10-10 09:18:13 +0300

    enemy selection

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,13 +1,22 @@
 import Card from './Card'
 
-export default function Cards({ veggies, onSelect }) {
-    
-
-    return (
-        <div className={'cards'}>
-            {veggies.map((veggie) => (
-                <Card key={veggie.id} veggie={veggie} onSelect={onSelect}/>
-            ) )}
-        </div>
-    )
+export default function Cards({ veggies, enemies,  onSelect, selectEnemy }) {
+    if (veggies) {
+        return (
+            <div className={'cards'}>
+                {veggies.map((veggie) => (
+                    <Card key={veggie.id} veggie={veggie} onSelect={onSelect}/>
+                ) )}
+            </div>
+        )
+    }
+    if (enemies) {
+        return (
+            <div className={'cards'}>
+                {enemies.map((enemy) => (
+                    <Card key={enemy.id} enemy={enemy} selectEnemy={selectEnemy}/>
+                ) )}
+            </div>
+        )
+    }
 }
