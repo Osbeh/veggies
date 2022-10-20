e989cca22769cc9bc0a4647af4fe7ea570e1a11c
ossarol <ossi.arola@qpr.com>
2022-10-20 20:48:37 +0300

    action log viimeistely

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,13 @@
 export default function Footer({ defeatedEnemies }) {
     console.log(defeatedEnemies)
     return (
-        <ol className="defeated">
-        {defeatedEnemies.map((enem) => (
-            <li key={enem.id}>{enem.name}</li>
-        ))}
-        </ol>
+        <div>
+            <h4>Defeated Vegetables</h4>
+            <ol className="defeated">
+            {defeatedEnemies.map((enem) => (
+                <li key={enem.id}>{enem.name}</li>
+            ))}
+            </ol>
+        </div>
     )
 }
