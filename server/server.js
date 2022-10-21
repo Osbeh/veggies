d88d3c840f376700f151261ab83c04c6fea3f0d6
ossarol <ossi.arola@qpr.com>
2022-10-21 12:27:34 +0300

    server start

---
+++ server/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+const app = express()
+import { insertMongo } from './insertMongo.js'
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => console.log("listeninig on port ", port))
+
+// const { MongoClient } = require("mongodb")
+
+
+// const mUrl = "mongodb://0.0.0.0:27017"
+
+// const client = new MongoClient(mUrl)
+
+// async function run() {
+//     try {
+//         const db = client.db("veggies")
+//         const fruits = db.collection("fruits")
+//         const fruit = await fineliFetch()
+//         const result = await fruits.insertOne(fruit)
+//         console.log('Doc inserted')
+//     } finally {
+//         await client.close()
+//     }
+// }
+
+// const fineliFetch = async () => {
+//     const res = await fetch('https://fineli.fi/fineli/api/v1/foods/11060');
+//     const json = await res.json();
+//     return json;
+// }
+
+// run().catch(console.dir)
+
+
+
