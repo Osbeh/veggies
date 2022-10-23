60b29127b013b2ab9615ac24fbbaa162538b28e6
ossarol <ossi.arola@qpr.com>
2022-10-23 20:54:17 +0300

    bäkkärin jatkoa

--- server/routes/fruits.js
+++ server/routes/fruits.js
@@ -1,20 +1,49 @@
 // import express from "express";
 const express = require('express')
+const { collection } = require('../models/veg')
 const router = express.Router()
+const Fruit = require('../models/veg')
 
 //Get all
-router.get('/', (req,res) => {
-    res.send('Hello')
+router.get('/', async (req,res) => {
+    try {
+        const fruits = await Fruit.find({ vegType: 'fruit' })
+        res.json(fruits)
+    } catch (err){
+        res.status(500).json({ message: err.message})
+    }
 })
 // Get one
 router.get('/:id', (req,res) => {
-
+    res.send(req.params.id)
 })
 
 //create one
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
+    const fruit = new Fruit({
+        name: req.body.name,
+        id: req.body.id,
+        energyKcal: req.body.energyKcal,
+        protein: req.body.protein,
+        fat: req.body.fat,
+        carbohydrate: req.body.carbohydrate,
+        vegType: 'fruit'
+    })
+
+    try {
+        const newFruit = await fruit.save()
+        res.status(201).json(newFruit)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
 
 })
+
+// async function getFruits(req, res, next) {
+//     try {
+//         fruits = await Fruit.find({ vegType:'fruits' }).exec()
+//     }
+// }
 
 //delete
 
